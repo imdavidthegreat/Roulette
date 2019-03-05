84ffb214615428237a9ce331d4bf992c275c954d
imdavidthegreat <davidegley88@gmail.com>
2019-03-05 13:53:53 -0800

    Finished bets implementation, removed split bets.

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -119,7 +119,192 @@ namespace Roulette
             }
 
         }
-        //
+
+        static int[] MakeColumnsBet()
+        {
+            int[] firstColumnNums = new int[12] { 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34 };
+            int[] secondColumnNums = new int[12] { 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35};
+            int[] thirdColumnNums = new int[12] { 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36};
+            int selection;
+            Console.WriteLine("Would you like to bet for First, Second, or Third?\n1. First\n2. Second\n3. Third");
+            selection = GetNum();
+
+            switch (selection)
+            {
+                case 1:
+                    return firstColumnNums;
+                case 2:
+                    return secondColumnNums;
+                default:
+                    return thirdColumnNums;
+
+            }
+
+        }
+
+        static int[] MakeStreetsBet()
+        {
+            int[] firstStreetNums = new int[3] { 1,2,3 };
+            int[] secondStreetNums = new int[3] { 4,5,6 };
+            int[] thirdStreetNums = new int[3] { 7,8,9 };
+            int[] fourthStreetNums = new int[3] { 10,11,12 };
+            int[] fifthStreetNums = new int[3] { 13,14,15 };
+            int[] sixthStreetNums = new int[3] { 16,17,18 };
+            int[] seventhStreetNums = new int[3] { 19,20,21 };
+            int[] eighthStreetNums = new int[3] { 22,23,24 };
+            int[] ninethStreetNums = new int[3] { 25,26,27 };
+            int[] tenthStreetNums = new int[3] { 28,29,30 };
+            int[] eleventhStreetNums = new int[3] { 31,32,33 };
+            int[] twelvethStreetNums = new int[3] { 34,35,36 };
+            int selection;
+            Console.WriteLine("Select an option based on the number that the Street you want to bet on starts with.\n1. 1\n2. 4\n3. 7" +
+                "\n4. 10\n5. 13\n6. 16\n7. 19\n8. 22\n9. 25\n10. 28\n11. 31\n12. 34");
+            selection = GetNum();
+
+            switch (selection)
+            {
+                case 1:
+                    return firstStreetNums;
+                case 2:
+                    return secondStreetNums;
+                case 3:
+                    return secondStreetNums;
+                case 4:
+                    return secondStreetNums;
+                case 5:
+                    return secondStreetNums;
+                case 6:
+                    return secondStreetNums;
+                case 7:
+                    return secondStreetNums;
+                case 8:
+                    return secondStreetNums;
+                case 9:
+                    return secondStreetNums;
+                case 10:
+                    return secondStreetNums;
+                case 11:
+                    return secondStreetNums;
+                default:
+                    return twelvethStreetNums;
+
+            }
+
+        }
+
+        static int[] Make6NumbersBet()
+        {
+            int[] oneandtwoStreetNums = new int[6] { 1, 2, 3, 4, 5, 6 };
+            int[] threeandfourStreetNums = new int[6] { 7, 8, 9, 10, 11, 12 };
+            int[] fiveandsixStreetNums = new int[6] { 13, 14, 15, 16, 17, 18 };
+            int[] sevenandeightStreetNums = new int[6] { 19, 20, 21, 22, 23, 24 };
+            int[] nineandtenStreetNums = new int[6] { 25, 26, 27, 28, 29, 30 };
+            int[] elevenandtwelveStreetNums = new int[6] { 31, 32, 33, 34, 35, 36 };
+            
+            int selection;
+            Console.WriteLine("Select an option based on the number that the pair of Streets you want to bet on starts with.\n1. 1\n2. 7" +
+                "\n3. 13\n4. 19\n5. 25\n6. 31");
+            selection = GetNum();
+
+            switch (selection)
+            {
+                case 1:
+                    return oneandtwoStreetNums;
+                case 2:
+                    return threeandfourStreetNums;
+                case 3:
+                    return fiveandsixStreetNums;
+                case 4:
+                    return sevenandeightStreetNums;
+                case 5:
+                    return nineandtenStreetNums;
+                default:
+                    return elevenandtwelveStreetNums;
+                
+
+            }
+
+        }
+
+        static int[] MakeCornersBet()
+        {
+            int[] oneCornertNums = new int[4] { 1, 2, 4, 5 };
+            int[] twoCornertNums = new int[4] { 2, 3, 5, 6 };
+            int[] fourCornertNums = new int[4] { 4, 5, 7, 8 };
+            int[] fiveCornertNums = new int[4] { 5, 6, 8, 9 };
+            int[] sevenCornertNums = new int[4] { 7, 8, 10, 11 };
+            int[] eightCornertNums = new int[4] { 8, 9, 11, 12 };
+            int[] tenCornertNums = new int[4] { 10, 11, 13, 14 };
+            int[] elevenCornertNums = new int[4] { 11, 12, 14, 15 };
+            int[] thirteenCornertNums = new int[4] { 13, 14, 16, 17 };
+            int[] fourteenCornertNums = new int[4] { 14, 15, 17, 18 };
+            int[] sixteenCornertNums = new int[4] { 16, 17, 19, 20 };
+            int[] seventeenCornertNums = new int[4] { 17, 18, 20, 21 };
+            int[] nineteenCornertNums = new int[4] { 19, 20, 22, 23 };
+            int[] twentyCornertNums = new int[4] { 20, 21, 23, 24 };
+            int[] twentytwoCornertNums = new int[4] { 22, 23, 25, 26 };
+            int[] twentythreeCornertNums = new int[4] { 23, 24, 26, 27 };
+            int[] twentyfiveCornertNums = new int[4] { 25, 26, 28, 29 };
+            int[] twentysixCornertNums = new int[4] { 26, 27, 29, 30 };
+            int[] twentyeightCornertNums = new int[4] { 28, 29, 31, 32 };
+            int[] twentynineCornertNums = new int[4] { 29, 30, 32, 33 };
+            int[] thirtyoneCornertNums = new int[4] { 31, 32, 34, 35 };
+            int[] thirtytwoCornertNums = new int[4] { 32, 33, 35, 36 };
+            
+            int selection;
+            Console.WriteLine("What is the bottom-left number of your corner (ex. 1 is the bottom left for the 1,2,4,5 corner).");
+            selection = GetNum();
+
+            switch (selection)
+            {
+                case 1:
+                    return oneCornertNums;
+                case 2:
+                    return twoCornertNums;
+                case 4:
+                    return fourCornertNums;
+                case 5:
+                    return fiveCornertNums;
+                case 7:
+                    return sevenCornertNums;
+                case 8:
+                    return eightCornertNums;
+                case 10:
+                    return tenCornertNums;
+                case 11:
+                    return elevenCornertNums;
+                case 13:
+                    return thirteenCornertNums;
+                case 14:
+                    return fourteenCornertNums;
+                case 16:
+                    return sixteenCornertNums;
+                case 17:
+                    return seventeenCornertNums;
+                case 19:
+                    return nineteenCornertNums;
+                case 20:
+                    return twentyCornertNums;
+                case 22:
+                    return twentytwoCornertNums;
+                case 23:
+                    return twentythreeCornertNums;
+                case 25:
+                    return twentyfiveCornertNums;
+                case 26:
+                    return twentysixCornertNums;
+                case 28:
+                    return twentyeightCornertNums;
+                case 29:
+                    return twentynineCornertNums;
+                case 31:
+                    return thirtyoneCornertNums;
+                default:
+                    return thirtytwoCornertNums;
+
+            }
+
+        }
 
         static void Menu()
         {
@@ -160,6 +345,7 @@ namespace Roulette
                         bets[i] = MakeEvenOddBet();
                         betNames[i] = "Even/Odd Bet";
                         betnumber++;
+                        break;
                     case 3:
                         bets[i] = MakeRedBlackBet();
                         betNames[i] = "Red/Black Bet";
@@ -175,8 +361,31 @@ namespace Roulette
                         betNames[i] = "Dozens Bet";
                         betnumber++;
                         break;
-
-
+                    case 6:
+                        bets[i] = MakeColumnsBet();
+                        betNames[i] = "Columns Bet";
+                        betnumber++;
+                        break;
+                    case 7:
+                        bets[i] = MakeStreetsBet();
+                        betNames[i] = "Streets Bet";
+                        betnumber++;
+                        break;
+                    case 8:
+                        bets[i] = Make6NumbersBet();
+                        betNames[i] = "6 Numbers Bet";
+                        betnumber++;
+                        break;
+                    case 9:
+                        bets[i] = MakeCornersBet();
+                        betNames[i] = "Corners Bet";
+                        betnumber++;
+                        break;
+                    case 10:
+                        bets[i] = MakeColumnsBet();
+                        betNames[i] = "Columns Bet";
+                        betnumber++;
+                        break;
                     default:
                         break;
                 }
